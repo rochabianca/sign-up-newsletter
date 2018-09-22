6f4e6615f5aba3d043ddd07ab48b5bc326eebaae
Bianca <bianca.rocha1711@gmail.com>
2018-09-22 14:57:20 -0300

    improviments on the layout of the first step

---
+++ src/components/templates/Input.js
@@ -0,0 +1,35 @@
+import React from "react";
+import propTypes from "prop-types";
+import classnames from "classnames";
+
+const Input = ({ name, label, type, placeholder, value, onChange, error }) => {
+  return (
+    <div className={classnames("input", { "input--invalid": error })}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      {error && <div className="input--danger">{error}</div>}
+    </div>
+  );
+};
+
+Input.propTypes = {
+  name: propTypes.string.isRequired,
+  label: propTypes.string.isRequired,
+  placeholder: propTypes.string.isRequired,
+  type: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  error: propTypes.string
+};
+
+Input.defaultProps = {
+  type: "text"
+};
+
+export default Input;
--- src/components/templates/InputBox.js
+++ src/components/templates/InputBox.js
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
+import Input from "./Input";
 
 class InputBox extends Component {
+  state = {
+    name: "",
+    email: "",
+    age: "",
+    error: {}
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  hasError = (item, name) => {
+    var errors = false;
+    if (item === "") errors = true;
+
+    if (errors) {
+      console.log(item);
+      this.setState({ error: { [name]: `${name} is required` } });
+      return true;
+    } else {
+      this.setState({ error: {} });
+      return false;
+    }
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const { name, age } = this.state;
+    this.hasError(name, "name");
+    this.hasError(age, "age");
+
+    if (!this.hasError(name, "name") && !this.hasError(age, "age")) {
+      const newRecipient = {
+        name,
+        age
+      };
+    }
+  };
+
   render() {
+    const { name, age, error } = this.state;
     return (
-      <div className="col-sm-5 col-md-5 col-lg-5 col-xl-5 input-box">
-        <h1>Helou</h1>
+      <div className="input-box col-sm-5 col-md-6 col-lg-5 col-xl-5 ">
+        <h3 className="input-box__title">Sign up to Our Newsletter</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <Input
+            label="name"
+            name="name"
+            placeholder="Your name"
+            value={name}
+            onChange={this.onChange}
+            error={error.name}
+          />
+          <Input
+            label="age"
+            name="age"
+            placeholder="Your age"
+            value={age}
+            onChange={this.onChange}
+            error={error.age}
+          />
+          <input type="submit" value="next" className="button button__submit" />
+        </form>
       </div>
     );
   }
